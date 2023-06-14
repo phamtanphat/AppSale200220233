f0e3db021cdd8a1f8c8d398347ddde9608aa540f
Phat <phatdroid94@gmail.com>
2023-06-14 21:15:35 +0700

    app resource

---
+++ app/src/main/java/com/example/appsale200220233/presentation/activity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appsale200220233.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appsale200220233.R
+import com.example.appsale200220233.data.remote.AppResource
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
